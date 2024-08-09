e8e524b27e33a3e98d70c4257ed2b56e3f7791dc
nonameplz34 <minhavez52@gmail.com>
2024-08-09 11:22:56 -0300

    vamboraa

---
+++ meu.js
@@ -0,0 +1,20 @@
+
+function colabora (){
+    x= "marmelada"
+    confirm(`ok? tudo bem  ${x}`);
+    
+    }
+    
+    function teste(){
+        confirm("are u heuman?")
+        
+        
+    }
+
+
+var x = 4
+let w = "4"
+const z = true
+
+
+console.log(typeof teste);
